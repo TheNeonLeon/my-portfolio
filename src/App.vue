381ef146e866d8436db591ca20fe7790c23b6c6a
TheNeonLeon <leon.listo@hotmail.com>
2021-03-03 18:37:32 +0100

    upd

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div data-aos="fade-right" data-aos-duration="1000" id="wrapper">
+      <h1 class="name">Hello my name is Leon Listo</h1>
+      <h1 class="title-info">I'm a Front-end developer</h1>
+      <button class="title-btn"><h2>Know more</h2> </button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data: () => {
+    return {
+
+    }
+  },
+  methods: {
+   
+    }
   }
-}
 </script>
 
 <style>
@@ -22,7 +31,43 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #272342;
   margin-top: 60px;
 }
+#wrapper{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  gap: 0px 0px;
+     animation: fadein 2s; 
+}
+
+.name{
+  grid-column: 2/2;
+    grid-row: 4 / 4;
+}
+.title-info{
+  grid-column: 2/2;
+  grid-row: 5 / 5;
+  margin-top: -3%;
+  margin-left: -8.5%;
+}
+.title-btn{
+    cursor: pointer;
+  grid-column: 2/2;
+  grid-row: 6 / 6;
+  background-color: white;
+  border-style: solid;
+  border-color: black;
+  margin-right: 55%;
+  margin-left: 5%;
+
+
+}
+.title-btn::after{
+    cursor: pointer;
+  background-color: linear-gradient(#000,#434343);
+
+}
+
 </style>
