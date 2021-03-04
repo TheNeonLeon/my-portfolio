3b6b44c9b29cc1be0a116edf0d82df1f020dbdfe
Le0n <leon.listo@hotmail.com>
2021-03-04 08:47:17 +0100

    about update

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <div id="wrapper">
       <h1 class="name">Hello my name is Leon Listo</h1>
       <h1 class="title-info">I'm a Front-end developer</h1>
-      <button v-scroll-to="'#about'" class="title-btn"><h2>Know more</h2> </button>
+      <span v-scroll-to="'#about'" class="title-btn"><h3 class="
+      know-more-text">Know more</h3> </span>
     </div>
       <About />
   </div>
@@ -28,6 +29,10 @@ export default {
 </script>
 
 <style>
+body{
+    padding: 0;
+margin: -1%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -35,6 +40,8 @@ export default {
   text-align: center;
   color: #272342;
   margin-top: 60px;
+  padding: 0;
+margin: 0;
 }
 #wrapper{
   display: grid;
@@ -46,24 +53,25 @@ export default {
 
 .name{
   grid-column: 2/2;
-    grid-row: 4 / 4;
+    grid-row: 3 / 3;
 }
 .title-info{
   grid-column: 2/2;
-  grid-row: 5 / 5;
+  grid-row: 4 / 4;
   margin-top: -3%;
   margin-left: -8.5%;
 }
 .title-btn{
     cursor: pointer;
   grid-column: 2/2;
-  grid-row: 6 / 6;
+  grid-row: 5 / 5;
   background-color: white;
   border-style: solid;
   border-color: black;
   margin-right: 55%;
   margin-left: 5%;
-
+  width: 40%;
+  height: 40%;
 
 }
 .title-btn::after{
@@ -71,5 +79,7 @@ export default {
   background-color: linear-gradient(#000,#434343);
 
 }
-
+.know-more-text{
+  margin-top: 10%;
+}
 </style>
--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="about">
       <h1>ABOUT ME</h1>
+      <h4 class="about-text">I'm a passionate Front-end developer who loves converting new ideas and concepts into code.
+        I'm a visual person with an eye for good and modern design.
+      </h4>
     </div>
 </template>
 
@@ -18,11 +21,17 @@ export default {
 #about{
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-  grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
   gap: 0px 0px;
+  border-style: solid;
+  color: white;
+  background-image: linear-gradient(
+135deg
+,#000,#434343);
+
 }
 h1{
-  grid-row: 4/4;
+  grid-row: 1/1;
   grid-column: 3/3;
 }
 h3 {
@@ -38,6 +47,10 @@ li {
 }
 a {
   color: #42b983;
+}
+.about-text{
+  grid-row: 3/3;
+  grid-column: 4/4;
 }
 
 </style>
