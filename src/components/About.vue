59d5941620d6f5223052bd78fcbf30552b353901
TheNeonLeon <leon.listo@hotmail.com>
2021-03-04 09:36:30 +0100

    upd about

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,6 +4,7 @@
       <h4 class="about-text">I'm a passionate Front-end developer who loves converting new ideas and concepts into code.
         I'm a visual person with an eye for good and modern design.
       </h4>
+      <img class="portrait" v-bind:src="require(`../assets/portrait.png`)">
     </div>
 </template>
 
@@ -21,7 +22,7 @@ export default {
 #about{
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-  grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: 0.5fr 0.5fr;
   gap: 0px 0px;
   border-style: solid;
   color: white;
@@ -49,8 +50,18 @@ a {
   color: #42b983;
 }
 .about-text{
-  grid-row: 3/3;
-  grid-column: 4/4;
+  grid-row: 2/2;
+  grid-column: 3/3;
+  padding-left: 20%;
+   width: 150%;
+  height: 100%;
+}
+.portrait{
+  grid-row: 2/2;
+  grid-column: 2/2;
+  width: 100%;
+  height: 100%;
+
 }
 
 </style>
